932365b37d9b7f97ca2e3adb7341ccff9b542e83
Zahidul Islam <tawraat@pop-os.localdomain>
2020-11-24 14:07:04 +0600

    contact mail via sendgrid

--- src/controller/contact.js
+++ src/controller/contact.js
@@ -1,5 +1,26 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.email);
+
 const contact = (req,res) => {
     res.render('contact')
 }
 
+const sendMail = (req,res) => {
+    const msg = {
+        to: 'tawraat0187@gmail.com',
+        from: `${req.body.name}@dcodelogic.com`,
+        subject: 'From a client',
+        text: req.body.message,
+      }
+      sgMail
+        .send(msg)
+        .then(() => {
+          res.send("Thanks for sendig email!");
+        })
+        .catch((error) => {
+          res.send("An Error occured!");
+        })
+}
+
 exports.contact = contact
+exports.sendMail = sendMail
--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const route = express.Router();
-const { contact } = require('../controller/contact')
+const { contact,sendMail } = require('../controller/contact');
 
 route.get('/', contact)
+route.post('/', sendMail);
 
 module.exports = route
